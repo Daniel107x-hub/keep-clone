3a5ad9348f455c7aae016433b821262b1a7570b1
daniel107x-hub <dmejiav310796@outlook.com>
2025-01-02 12:52:44 -0600

    Added unique indexes to email and username

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,7 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Models
 {
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(UserName), IsUnique = true)]
     public class User{
         [Key]
         public long Id { get; set; }

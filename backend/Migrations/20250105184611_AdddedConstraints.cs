ce480dd3cec4da95d7639972c0c9cae591cdffeb
Daniel107x-hub <DMejiaV310796@outlook.com>
2025-01-05 13:46:42 -0600

    Added missing migrations

---
+++ backend/Migrations/20250105184611_AdddedConstraints.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdddedConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Reminders",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserName",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Reminders",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}

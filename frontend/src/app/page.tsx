89202168b8dbce1b0899992bc65db71d3753c868
daniel107x-hub <dmejiav310796@outlook.com>
2025-01-08 13:40:55 -0600

    Removed additional call

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,6 @@
 "use client"
-import {useEffect} from "react";
-import axios from "axios";
-
-axios.defaults.baseURL = 'http://localhost:5085/api';
 
 export default function Home() {
-  useEffect(() => {
-    const activate = async function(){
-      const response = await axios.get("/Account/Activate/wewe");
-      console.log(response);
-    }
-    activate();
-  }, [])
   return (
       <>Hello</>
   );

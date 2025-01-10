e789330d6c0db2b3a1153869e82d77c777518584
daniel107x-hub <dmejiav310796@outlook.com>
2025-01-10 11:18:12 -0600

    Added ID to note DTO

--- backend/Models/NoteDto.cs
+++ backend/Models/NoteDto.cs
@@ -1,6 +1,7 @@
 namespace backend.Models
 {
     public class NoteDto {
+        public long Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
     }   
